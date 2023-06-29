6cb4bac3795fce92934a2d0a9d6f763031271752
sainathaluri <alurusainath85@gmail.com>
2023-06-29 19:31:29 +0530

    HR Frontend without css

---
+++ src/PurchaseOrder/PurchaseOrder.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function PurchaseOrder() {
+  const [users, setUsers] = useState([]);
+  const [userDetail, setUserDetail] = useState({});
+  let location = useLocation();
+  const { employeeId } = location.state;
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get(
+      `http://localhost:8082/employees/${employeeId}/orders`
+    );
+    const details = await axios.get(`http://localhost:8082/employees/${employeeId}`);
+    console.log(details);
+    setUsers(result.data);
+    setUserDetail({ first: details.data.firstName, last: details.data.lastName });
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4">
+      {userDetail.first} {userDetail.last}
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">S.No</th>
+              <th scope="col">Date Of Joining</th>
+              <th scope="col">Project End Date</th>
+              <th scope="col">Client Name</th>
+              <th scope="col">Vendor PhoneNo</th>
+              <th scope="col">Vendor Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length > 0 ? (
+              users.map((employeePO, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{employeePO.dateOfJoining}</td>
+                  <td>{employeePO.projectEndDate}</td>
+                  <td>{employeePO.endClientName}</td>
+                  <td>{employeePO.vendorPhoneNo}</td>
+                  <td>{employeePO.vendorEmailId}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="6">No Orders</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+// import React, { useEffect, useState } from "react";
+// import { useLocation, useParams } from "react-router-dom";
+// import axios from "axios";
+
+// export default function Home() {
+//   const [users, setUsers] = useState([]);
+//   const [userDetail, setUserDetail] = useState({});
+//   let location = useLocation();
+//   const { empId } = useParams();
+  
+
+//   useEffect(() => {
+//     loadUsers();
+//     setUsers(location.state);
+  
+//   }, []);
+  
+
+//   const loadUsers = async () => {
+//     const result = await axios.get(`http://localhost:8082/employees/${empId}/orders`);
+//     const details = await axios(`http://localhost:8082/employees/${empId}`);
+//     console.log(details);
+//     setUsers(result.data);
+//     setUserDetail({first : details.data.firstName, last : details.data.lastName})
+//   };
+
+//   return (
+//     <div className="container">
+//       <div className="py-4">
+//         <table className="table border shadow">
+//             {userDetail.first} {userDetail.last}
+//           <thead>
+//             <tr>
+//               <th scope="col">S.No</th>
+//               <th scope="col">Date Of Joining</th>
+//               <th scope="col">Project End Date</th>
+//               <th scope="col">Client Name</th>
+//               <th scope="col">Vendor PhoneNo</th>
+//               <th scope="col">Vendor Email</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+          
+//             { users.map((employeePO, index) => (
+//               <tr>
+//                 <th scope="row" key={index}>
+//                   {index + 1}
+//                 </th>
+//                 <td>{employeePO.dateOfJoining}</td>
+//                 <td>{employeePO.projectEndDate}</td>
+//                 <td>{employeePO.endClientName}</td>
+//                 <td>{employeePO.vendorPhoneNo}</td>
+//                 <td>{employeePO.vendorEmailId}</td>
+//                 </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// }
---
+++ src/pages/Home.js
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import axios from "axios";
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  const [showAddOrdersLink, setShowAddOrdersLink] = useState(false);
+
+  useEffect(() => {
+    const result = axios.get("http://localhost:8082/employees");
+    result
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleViewOrders = (employeeId) => {
+    navigate(`/orders`, { state: { employeeId } });
+    setShowAddOrdersLink(true);
+  };
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">S.No</th>
+              <th scope="col">FirstName</th>
+              <th scope="col">LastName</th>
+              <th scope="col">EmailID</th>
+              <th scope="col">Visa Status</th>
+              <th scope="col">Date Of Birth</th>
+              <th scope="col">College Graduation</th>
+              <th scope="col">Visa StartDate</th>
+              <th scope="col">Visa EndDate</th>
+              <th scope="col">Working Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length > 0 ? (
+              users.map((employee, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.emailID}</td>
+                  <td>{employee.visaStatus}</td>
+                  <td>{employee.dob}</td>
+                  <td>{employee.clgOfGrad}</td>
+                  <td>{employee.visaStartDate}</td>
+                  <td>{employee.visaExpiryDate}</td>
+                  <td>{employee.onBench ? "Yes" : "No"}</td>
+                  {employee.length === 0 && (
+                    <tr>
+                      <td colSpan="8">No record found</td>
+                    </tr>
+                  )}
+                  <td>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => handleViewOrders(employee.employeeID)}
+                    >
+                      View Orders
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="10">No users found</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      {showAddOrdersLink && (
+        <NavLink className="btn btn-outline-light" to="/addorders">
+          Add Orders
+        </NavLink>
+      )}
+    </div>
+  );
+}
+
+
+// import React, { useEffect, useState } from "react";
+// import {useNavigate , NavLink} from "react-router-dom";
+// import axios from "axios";
+
+// export default function Home() {
+//   const [users, setUsers] = useState([]);
+//   let navigate = useNavigate();
+
+//   useEffect(() => {
+//     const result =  axios.get("http://localhost:8082/employees");
+//     result.then((response) => {
+//       setUsers(response.data);
+//     }).catch(error => console.log(error));
+//   }, []);
+
+
+
+//   return (
+//     <div className="container">
+//       <div className="py-4">
+//         <table className="table border shadow">
+//           <thead>
+//             <tr>
+//               <th scope="col">S.No</th>
+//               <th scope="col">FirstName</th>
+//               <th scope="col">LastName</th>
+//               <th scope="col">EmailID</th>
+//               <th scope="col">Visa Status</th>
+//               <th scope="col">Date Of Birth</th> 
+//               <th scope="col">College Graduation</th>
+//               <th scope="col">Visa StartDate</th>
+//               <th scope="col">Visa EndDate</th>
+//               <th scope="col">Working Status</th>
+//               {/* <th scope="col">Action</th> */}
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {users && users.length > 0 && users.map((employee, index) => {
+//               return(
+//               <tr key={Math.random()}>
+//                 <th scope="row" key={index}>
+//                   {index+1}
+//                 </th>
+//                 <td>{employee.firstName}</td>
+//                 <td>{employee.lastName}</td>
+//                 <td>{employee.emailID}</td>
+//                 <td>{employee.visaStatus}</td>
+//                 <td>{employee.dob}</td>
+//                 <td>{employee.clgOfGrad}</td>
+//                 <td>{employee.visaStartDate}</td>
+//                 <td>{employee.visaExpiryDate}</td>
+//                 <td>{employee.onBench ? 'Yes' : 'No'}</td>
+//                 {
+//                 !employee.length ===  0  && 
+//                 <tr>
+//                     <td colSpan='8'> No record found</td>
+//                 </tr>
+//             }
+//               {/* <NavLink to={`/orders/${employee.employeeID}`}>View Orders </NavLink> */}
+
+//               <NavLink to={`/orders`} state={{ employeeId: employee.employeeID }}>View Orders</NavLink>
+
+//               </tr>
+// )})}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// }
