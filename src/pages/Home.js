61fadb8ff3ec65c03e7855370c739ca965772fc0
Sainath <92996163+sainathaluri@users.noreply.github.com>
2023-09-25 23:33:23 +0530

    commiting Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ import { FiEdit2 } from "react-icons/fi";
 import { AiFillDelete, AiOutlineUsergroupAdd } from "react-icons/ai";
 import { BsFillPersonPlusFill } from "react-icons/bs";
 import Pagination from "../pages/Pagination";
+import { Select, Input, Space, Button } from "antd";
 
 export default function Home() {
   const apiUrl = process.env.REACT_APP_API_URL;
@@ -15,17 +16,27 @@ export default function Home() {
   const [currentPage, setCurrentPage] = useState(0);
   const [pageSize, setPageSize] = useState(10);
   const [totalPages, setTotalPages] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchField, setSearchField] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     fetchData();
-  }, [currentPage, pageSize]);
+  }, [currentPage, pageSize, searchQuery, searchField]);
 
   const fetchData = async () => {
     try {
       const token = localStorage.getItem("token");
+      const searchParams = new URLSearchParams();
+      searchParams.append("page", currentPage);
+      searchParams.append("size", pageSize);
+      if (searchQuery) {
+        searchParams.append("searchField", searchField);
+        searchParams.append("searchString", searchQuery);
+      }
+
       const response = await fetch(
-        `${apiUrl}/employees?page=${currentPage}&size=${pageSize}`,
+        `${apiUrl}/employees?${searchParams.toString()}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -48,7 +59,7 @@ export default function Home() {
   const handleViewOrders = (employeeId) => {
     navigate(`/orders/${employeeId}`);
   };
-  
+
   const handleViewTracking = (employeeId) => {
     navigate(`/tracking/${employeeId}`);
   };
@@ -74,20 +85,55 @@ export default function Home() {
     }
   };
 
+  const handleSearch = () => {
+    fetchData();
+  };
+
+  const handleClearSearch = () => {
+    setSearchQuery("");
+    setSearchField("");
+    fetchData();
+  };
+
   return (
     <div className="container">
       <div className="py-4">
         <h4 className="text-center">Employee details</h4>
         <div className="add-orders d-flex justify-content-end">
-        <Link className="add-user-link" to="/adduser">
-            <BsFillPersonPlusFill size={25} title="Add Employee"/>
+          <Link className="add-user-link" to="/adduser">
+            <BsFillPersonPlusFill size={25} title="Add Employee" />
           </Link>
         </div>
         <div className="add-orders d-flex justify-content-end">
           <Link className="add-pro-link" to="/addprospect">
-            <AiOutlineUsergroupAdd size={25} title="Prospect Employee"/>
+            <AiOutlineUsergroupAdd size={25} title="Prospect Employee" />
           </Link>
+        </div>
+        <div className="search-container">
+          <div className="search-bar">
+            <Select
+              value={searchField}
+              onChange={(value) => setSearchField(value)}
+              style={{ width: 120 }}
+            >
+              <Select.Option value="">Select Field</Select.Option>
+              <Select.Option value="firstName">First Name</Select.Option>
+              <Select.Option value="lastName">Last Name</Select.Option>
+              <Select.Option value="emailID">Email Id</Select.Option>
+              <Select.Option value="phoneNo">Phone No</Select.Option>
+              <Select.Option value="onBench">Working Status</Select.Option>
+            </Select>
+            <Input.Search
+              placeholder="Search..."
+              onSearch={handleSearch}
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              enterButton
+            />
           </div>
+          <Button onClick={handleClearSearch}>Clear</Button>
+        </div>
+
         <table className="table border shadow">
           <thead>
             <tr>
@@ -100,7 +146,7 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {users.length > 0 ? (
+            {users && users.length > 0 ? (
               users.map((employee, index) => {
                 const userIndex = index + currentPage * pageSize;
                 return (
@@ -112,9 +158,11 @@ export default function Home() {
                     <td>{employee.phoneNo}</td>
                     <td>{employee.onBench}</td>
                     <td>
-                    <div className="icon-container">
+                      <div className="icon-container">
                         <FiEdit2
-                          onClick={() => handleEditEmployee(employee.employeeID)}
+                          onClick={() =>
+                            handleEditEmployee(employee.employeeID)
+                          }
                           size={20}
                           title="Edit Employee"
                         />
@@ -124,12 +172,16 @@ export default function Home() {
                           title="Purchase Orders"
                         />
                         <BiDollar
-                          onClick={() => handleViewTracking(employee.employeeID)}
+                          onClick={() =>
+                            handleViewTracking(employee.employeeID)
+                          }
                           size={20}
                           title="WithHold Tracking"
                         />
                         <AiFillDelete
-                          onClick={() => handleDeleteEmployee(employee.employeeID)}
+                          onClick={() =>
+                            handleDeleteEmployee(employee.employeeID)
+                          }
                           size={20}
                           className="delete-icon"
                           title="Delete"
@@ -146,7 +198,11 @@ export default function Home() {
             )}
           </tbody>
         </table>
-        <Pagination currentPage={currentPage} totalPages={totalPages} setCurrentPage={setCurrentPage}/>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </div>
   );
