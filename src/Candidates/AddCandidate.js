398f62159af565e90fb42a9be71945cf7422fc96
Sainath <92996163+sainathaluri@users.noreply.github.com>
2023-10-17 00:10:50 +0530

    Added changes password routes

--- src/Candidates/AddCandidate.js
+++ src/Candidates/AddCandidate.js
@@ -1,195 +1,22 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Modal } from "antd";
+import React, { useEffect, useState } from "react";
+import CandidateForm from "./CandidateForm";
+import fetchRecruiters from "./fetchRecruiters";
 
 export default function AddCandidate() {
-  const apiUrl = process.env.REACT_APP_API_URL;
-  let navigate = useNavigate();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [user, setUser] = useState({
-    firstName: "",
-    lastName: "",
-    emailAddress: "",
-    recruiterName: "",
-    skills: "",
-    phoneNo: "",
-    originalVisaStatus: "",
-    comments: "",
-    candidateStatus: "",
-  });
+  const [recruiters, setRecruiters] = useState([]);
 
-  const {
-    firstName,
-    lastName,
-    emailAddress,
-    recruiterName,
-    skills,
-    phoneNo,
-    originalVisaStatus,
-    comments,
-    candidateStatus,
-  } = user;
+  useEffect(() => {
+    const fetchRecruitersData = async () => {
+      const recruiterData = await fetchRecruiters();
+      setRecruiters(recruiterData);
+    };
 
-  const onInputChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  };
+    fetchRecruitersData();
+  }, []);
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const requestOptions = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-        body: JSON.stringify(user),
-      };
-
-      const response = await fetch(`${apiUrl}/candidates`, requestOptions);
-      console.log(response);
-      if (response.status === 201) {
-        showModal();
-      }
-    } catch (error) {
-      console.error("Error adding candidate:", error);
-    }
-  };
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-    navigate("/candidates");
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    navigate("/candidates");
-  };
   return (
-    <div className="form-container">
-      <h2 className="text-center m-4">Add Candidate</h2>
-
-      <form onSubmit={(e) => onSubmit(e)}>
-        <div className="form-row">
-          <div className="form-group">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type={"text"}
-              className="form-control"
-              name="firstName"
-              value={firstName}
-              onChange={(e) => onInputChange(e)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type={"text"}
-              className="form-control"
-              name="lastName"
-              value={lastName}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="emailAddress">Email Address</label>
-          <input
-            type={"text"}
-            className="form-control"
-            name="emailAddress"
-            value={emailAddress}
-            onChange={(e) => onInputChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="recruiterName">Recruiter Name</label>
-          <input
-            type={"text"}
-            className="form-control"
-            name="recruiterName"
-            value={recruiterName}
-            onChange={(e) => onInputChange(e)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="skills">Skills</label>
-          <input
-            type={"text"}
-            className="form-control"
-            name="skills"
-            value={skills}
-            onChange={(e) => onInputChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="phoneNo">Phone No</label>
-          <input
-            type={"number"}
-            className="form-control"
-            name="phoneNo"
-            value={phoneNo}
-            onChange={(e) => onInputChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="originalVisaStatus">Visa Status</label>
-          <input
-            type={"text"}
-            className="form-control"
-            name="originalVisaStatus"
-            value={originalVisaStatus}
-            onChange={(e) => onInputChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="comments">Comments</label>
-          <input
-            type={"text"}
-            className="form-control"
-            name="comments"
-            value={comments}
-            onChange={(e) => onInputChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="candidateStatus">Working or Bench</label>
-          <select
-            id="candidateStatus"
-            name="candidateStatus"
-            value={candidateStatus}
-            onChange={(e) => onInputChange(e)}
-            required
-          >
-            <option value="">-- Select --</option>
-            <option value="In Training">In Training</option>
-            <option value="InMarketing">In Marketing</option>
-            <option value="Needs Training">Needs Training</option>
-            <option value="Start Marketing">Start Marketing</option>
-            <option value="Stop Marketing">Stop Marketing</option>
-            <option value="Placed">Placed</option>
-            <option value="Resigned from company">Resigned from company</option>
-          </select>
-        </div>
-        <button type="submit" className="btn btn-outline-primary">
-          Submit
-        </button>
-        <Link className="btn btn-outline-danger mx-2" to="/">
-          Cancel
-        </Link>
-        <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-          <p>Candidate added succesfully</p>
-        </Modal>
-      </form>
+    <div>
+      <CandidateForm mode="add" recruiters={recruiters} />
     </div>
   );
 }
--- src/Candidates/CandidateForm.js
+++ src/Candidates/CandidateForm.js
@@ -1,216 +1,234 @@
-// import React, { useState, useEffect } from "react";
-// import { Modal } from "antd";
-// import { useNavigate, useParams, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Modal } from "antd";
+import { useNavigate, useParams, Link } from "react-router-dom";
 
-// export default function CandidateForm({ mode }) {
-//   const apiUrl = process.env.REACT_APP_API_URL;
-//   const navigate = useNavigate();
-//   let { candidateID } = useParams();
+export default function CandidateForm({ mode, recruiters }) {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const navigate = useNavigate();
+  let { candidateID } = useParams();
 
-//   const [user, setUser] = useState({
-//     firstName: "",
-//     lastName: "",
-//     emailAddress: "",
-//     recruiterName: "",
-//     skills: "",
-//     phoneNo: "",
-//     originalVisaStatus: "",
-//     comments: "",
-//     candidateStatus: "",
-//   });
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    recruiterName: "",
+    skills: "",
+    phoneNo: "",
+    originalVisaStatus: "",
+    comments: "",
+    candidateStatus: "",
+  });
 
-//   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-//   const { firstName, lastName, emailAddress, recruiterName, skills, phoneNo, originalVisaStatus, comments, candidateStatus } = user;
+  const { firstName, lastName, emailAddress, recruiterName, skills, phoneNo, originalVisaStatus, comments, candidateStatus } = user;
 
-//   useEffect(() => {
+  useEffect(() => {
+    if (mode === "edit" && candidateID) {
+      const fetchCandidateData = async () => {
+        try {
+          const token = localStorage.getItem("token");
+          const requestOptions = {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          };
 
-//   }, []);
+          const response = await fetch(`${apiUrl}/candidates/${candidateID}`, requestOptions);
 
-//   const onSubmit = async (event) => {
-//     event.preventDefault();
+          if (response.status === 200) {
+            const candidateData = await response.json();
+            setUser(candidateData);
+          }
+        } catch (error) {
+          console.error("Error fetching candidate data:", error);
+        }
+      };
+      fetchCandidateData();
+    }
+  }, [mode, candidateID]);
 
-//     try {
-//       const requestOptions = {
-//         method: mode === "edit" ? "PUT" : "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Bearer ${localStorage.getItem("token")}`,
-//         },
-//         body: JSON.stringify(user),
-//       };
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-//       const response = await fetch(`${apiUrl}/candidates${mode === "edit" ? `/${candidateID}` : ""}`, requestOptions);
+    try {
+      const requestOptions = {
+        method: mode === "edit" ? "PUT" : "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify(user),
+      };
 
-//       if (response.status === 200 || response.status === 201) {
-//         showModal();
-//       }
-//     } catch (error) {
-//       console.error(`Error ${mode === "edit" ? "updating" : "adding"} candidate:`, error);
-//     }
-//   };
+      const response = await fetch(`${apiUrl}/candidates${mode === "edit" ? `/${candidateID}` : ""}`, requestOptions);
 
-//   const showModal = () => {
-//     setIsModalOpen(true);
-//   };
+      if (response.status === 200 || response.status === 201) {
+        showModal();
+      }
+    } catch (error) {
+      console.error(`Error ${mode === "edit" ? "updating" : "adding"} candidate:`, error);
+    }
+  };
 
-//   const handleOk = () => {
-//     setIsModalOpen(false);
-//     navigate("/candidates");
-//   };
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
-//   const handleCancel = () => {
-//     setIsModalOpen(false);
-//     navigate("/candidates"); 
-//   };
+  const handleOk = () => {
+    setIsModalOpen(false);
+    navigate("/candidates");
+  };
 
-//   const onInputChange = (e) => {
-//     setUser({ ...user, [e.target.name]: e.target.value });
-//   };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    navigate("/candidates"); 
+  };
 
-//   const isEditMode = mode === "edit";
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-//   return (
-//     <div>
-//       <div className="form-container">
-//         <h2 className="text-center m-4">{isEditMode ? "Edit" : "Add"} Candidate</h2>
-//         <form onSubmit={(e) => onSubmit(e)}>
-//           <div className="form-row">
-//             <div className="form-group">
-//               <label htmlFor="firstName">First Name</label>
-//               <input
-//                 type="text"
-//                 className="form-control"
-//                 name="firstName"
-//                 value={firstName}
-//                 onChange={(e) => onInputChange(e)}
-//                 required
-//               />
-//             </div>
-//             <div className="form-group">
-//               <label htmlFor="lastName">Last Name</label>
-//               <input
-//                 type="text"
-//                 className="form-control"
-//                 name="lastName"
-//                 value={lastName}
-//                 onChange={(e) => onInputChange(e)}
-//               />
-//             </div>
-//           </div>
+  const isEditMode = mode === "edit";
 
-//           {/* Add other form fields here */}
-//           {/* For example: emailAddress, recruiterName, skills, phoneNo, originalVisaStatus, comments, candidateStatus, etc. */}
-//           <div className="form-group">
-//             <label htmlFor="emailAddress">Email</label>
-//             <input
-//               type="text"
-//               className="form-control"
-//               placeholder="Email Address"
-//               name="emailAddress"
-//               value={emailAddress}
-//               onChange={(e) => onInputChange(e)}
-//               required
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="recruiterName">Recruiter Name</label>
-//             <select
-//               className="form-control"
-//               name="recruiterName"
-//               value={recruiterName}
-//               onChange={(e) => onInputChange(e)}
-//               required
-//             >
-//               <option value="">-- Select --</option>
-//               {Array.isArray(recruiters) &&
-//                 recruiters.map((recruiter) => (
-//                   <option
-//                     key={recruiter.employeeID}
-//                     value={recruiter.firstName}
-//                   >
-//                     {recruiter.firstName} {recruiter.lastName}
-//                   </option>
-//                 ))}
-//             </select>
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="skills">Skills</label>
-//             <input
-//               type="text"
-//               className="form-control"
-//               placeholder="Skills"
-//               name="skills"
-//               value={skills}
-//               onChange={(e) => onInputChange(e)}
-//               required
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="clgOfGrad">Phone No</label>
-//             <input
-//               type="text"
-//               className="form-control"
-//               placeholder="Phone no"
-//               name="phoneNo"
-//               value={phoneNo}
-//               onChange={(e) => onInputChange(e)}
-//               required
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="originalVisaStatus">Visa Status</label>
-//             <input
-//               type={"text"}
-//               className="form-control"
-//               name="originalVisaStatus"
-//               value={originalVisaStatus}
-//               onChange={(e) => onInputChange(e)}
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="comments">Comments</label>
-//             <input
-//               type={"text"}
-//               className="form-control"
-//               name="comments"
-//               value={comments}
-//               onChange={(e) => onInputChange(e)}
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="candidateStatus">Candidate Status</label>
-//             <select
-//               id="candidateStatus"
-//               name="candidateStatus"
-//               value={candidateStatus}
-//               onChange={(e) => onInputChange(e)}
-//               required
-//             >
-//               <option value="">-- Select --</option>
-//               <option value="In Training">In Training</option>
-//               <option value="InMarketing">In Marketing</option>
-//               <option value="Needs Training">Needs Training</option>
-//               <option value="Start Marketing">Start Marketing</option>
-//               <option value="Stop Marketing">Stop Marketing</option>
-//               <option value="Placed">Placed</option>
-//               <option value="Resigned from company">
-//                 Resigned from company
-//               </option>
-//             </select>
-//           </div>
+  return (
+    <div>
+      <div className="form-container">
+        <h2 className="text-center m-4">{isEditMode ? "Edit" : "Add"} Candidate</h2>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="form-row">
+            <div className="form-group">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="firstName"
+                value={firstName}
+                onChange={(e) => onInputChange(e)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="lastName"
+                value={lastName}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="emailAddress">Email</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email Address"
+              name="emailAddress"
+              value={emailAddress}
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="recruiterName">Recruiter Name</label>
+            <select
+              className="form-control"
+              name="recruiterName"
+              value={recruiterName}
+              onChange={(e) => onInputChange(e)}
+              required={mode === "edit"}
+            >
+              <option value="">-- Select --</option>
+              {Array.isArray(recruiters) &&
+                recruiters.map((recruiter) => (
+                  <option
+                    key={recruiter.employeeID}
+                    value={recruiter.firstName}
+                  >
+                    {recruiter.firstName} {recruiter.lastName}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="skills">Skills</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Skills"
+              name="skills"
+              value={skills}
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="clgOfGrad">Phone No</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Phone no"
+              name="phoneNo"
+              value={phoneNo}
+              onChange={(e) => onInputChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="originalVisaStatus">Visa Status</label>
+            <input
+              type={"text"}
+              className="form-control"
+              name="originalVisaStatus"
+              value={originalVisaStatus}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="comments">Comments</label>
+            <input
+              type={"text"}
+              className="form-control"
+              name="comments"
+              value={comments}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="candidateStatus">Candidate Status</label>
+            <select
+              id="candidateStatus"
+              name="candidateStatus"
+              value={candidateStatus}
+              onChange={(e) => onInputChange(e)}
+              required
+            >
+              <option value="">-- Select --</option>
+              <option value="In Training">In Training</option>
+              <option value="InMarketing">In Marketing</option>
+              <option value="Needs Training">Needs Training</option>
+              <option value="Start Marketing">Start Marketing</option>
+              <option value="Stop Marketing">Stop Marketing</option>
+              <option value="Placed">Placed</option>
+              <option value="Resigned from company">
+                Resigned from company
+              </option>
+            </select>
+          </div>
 
-//           <button type="submit" className="btn btn-outline-primary">
-//             {isEditMode ? "Update" : "Submit"}
-//           </button>
-//           <Link className="btn btn-outline-danger mx-2" to="/candidates">
-//             Cancel
-//           </Link>
-//           <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-//             <p>Candidate {isEditMode ? "Updated" : "Added"} successfully</p>
-//           </Modal>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
+          <button type="submit" className="btn btn-outline-primary">
+            {isEditMode ? "Update" : "Submit"}
+          </button>
+          <Link className="btn btn-outline-danger mx-2" to="/candidates">
+            Cancel
+          </Link>
+          <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <p>Candidate {isEditMode ? "Updated" : "Added"} successfully</p>
+          </Modal>
+        </form>
+      </div>
+    </div>
+  );
+}
