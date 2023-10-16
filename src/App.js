1261cc57fe67e1a7df493a0319b2a5ddc6b60abb
Sainath <92996163+sainathaluri@users.noreply.github.com>
2023-10-17 02:59:28 +0530

    removed sidebar for changepassword component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import AddEmployee from "./Employee/AddEmployee";
 import PurchaseOrder from "./PurchaseOrder/PurchaseOrder";
 import AddOrder from "./PurchaseOrder/AddOrder";
@@ -61,98 +66,188 @@ function useLocalStorage(key, initialValue) {
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useLocalStorage("isLoggedIn", false);
-  
-  const[role,setRole] = useLocalStorage("role", "");
+  const [tempPassword, setTempPassword] = useLocalStorage("tempPassword", false);
+  const [role, setRole] = useLocalStorage("role", "");
 
   const handleLogin = (userRole) => {
-    if(localStorage.getItem("token")){
+    if (localStorage.getItem("token")) {
       setIsLoggedIn(true);
       setRole(userRole);
     }
   };
 
+  useEffect(() => {
+    const storedTempPassword = localStorage.getItem("tempPassword");
+    if (storedTempPassword === "true") {
+      setTempPassword(true);
+    } else {
+      setTempPassword(false);
+    }
+  }, [isLoggedIn]);
+
   return (
     <div className="App">
       <Router>
-        <Navbar location={window.location} setIsLoggedIn ={setIsLoggedIn} setRole ={setRole}/>
-        {isLoggedIn && <Breadcrumb/>}
+        <Navbar
+          location={window.location}
+          setIsLoggedIn={setIsLoggedIn}
+          setRole={setRole}
+        />
+        {isLoggedIn && <Breadcrumb />}
         <div className="container-fluid">
           <div className="row">
-          <div className="col-md-2 bg-light">
-              {isLoggedIn && (role === "ADMIN" || role === "RECRUITER" || role === "SALES") && <Sidebar/>}
+            <div className="col-md-2 bg-light">
+            {isLoggedIn && !tempPassword && (
+                (role === "ADMIN" ||
+                  role === "RECRUITER" ||
+                  role === "SALES") && <Sidebar />
+              )}
             </div>
             <div className="col-md-10"></div>
-        <Routes>
-          <Route path="/login" element={<Login onLogin={handleLogin} />} />
-          <Route path="/forgot-password" element={<ForgotPassword/>} />
-          {isLoggedIn ?(
-            role === "ADMIN" ? (
+            <Routes>
+              <Route path="/login" element={<Login onLogin={handleLogin} />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              {isLoggedIn ? (
+                role === "ADMIN" ? (
                   <>
                     <Route path="/" element={<Home />} />
                     <Route path="/adduser" element={<AddEmployee />} />
-                    <Route path="/editemployee/:employeeId" element={<EditEmployee />} />
-                    <Route path="/orders/:employeeId" element={<PurchaseOrder />} />
-                    <Route path="/orders/:employeeId/:orderId/editorder" element={<EditOrder />} />
-                    <Route path="/orders/:employeeId/addorder" element={<AddOrder />} />
-                    <Route path="/tracking/:employeeId" element={<WithHoldTracking />} />
-                    <Route path="/tracking/:employeeId/:trackingId/edittracking" element={<EditWithHoldTracking />} />
-                    <Route path="/tracking/:employeeId/addtracking" element={<AddWithHoldTracking />} />
-                    <Route path="/editemployee/:employeeId/project-history" element={<ProjectHistory />} />
-                    <Route path="/editemployee/:employeeId/project-history/:projectId/editproject" element={<EditProjectHistory />} />
-                    <Route path="/editemployee/:employeeId/project-history/add-project" element={<AddProjectHistory/>} />
-                    <Route path="/editemployee/:employeeId/visa-details" element={<VisaDetails/>} />
-                    <Route path="/editemployee/:employeeId/visa-details/:visaId/editvisadetails" element={<EditVisaDetails/>} />
-                    <Route path="/editemployee/:employeeId/visa-details/add-visa-details" element={<AddVisaDetails/>} />
-                    <Route path="/purchase-orders" element={<PurchaseOrders/>}/>
-                    <Route path="/change-password/:id" element={<ChangePasswordForm/>}/>
-                    <Route path="/addprospect" element={<AddProspectEmployee/>}/>
-                    <Route path="/candidates" element={<CandidateList/>}/>
-                    <Route path="/addcandidate" element={<AddCandidate/>}/>
-                    <Route path="/editcandidate/:candidateID" element={<EditCandidate/>}/>
-                    <Route path="/marketing" element={<MarketingList/>}/>
-                    <Route path="/marketing/editcandidate/:candidateID" element={<EditCandidate/>}/>
+                    <Route
+                      path="/editemployee/:employeeId"
+                      element={<EditEmployee />}
+                    />
+                    <Route
+                      path="/orders/:employeeId"
+                      element={<PurchaseOrder />}
+                    />
+                    <Route
+                      path="/orders/:employeeId/:orderId/editorder"
+                      element={<EditOrder />}
+                    />
+                    <Route
+                      path="/orders/:employeeId/addorder"
+                      element={<AddOrder />}
+                    />
+                    <Route
+                      path="/tracking/:employeeId"
+                      element={<WithHoldTracking />}
+                    />
+                    <Route
+                      path="/tracking/:employeeId/:trackingId/edittracking"
+                      element={<EditWithHoldTracking />}
+                    />
+                    <Route
+                      path="/tracking/:employeeId/addtracking"
+                      element={<AddWithHoldTracking />}
+                    />
+                    <Route
+                      path="/editemployee/:employeeId/project-history"
+                      element={<ProjectHistory />}
+                    />
+                    <Route
+                      path="/editemployee/:employeeId/project-history/:projectId/editproject"
+                      element={<EditProjectHistory />}
+                    />
+                    <Route
+                      path="/editemployee/:employeeId/project-history/add-project"
+                      element={<AddProjectHistory />}
+                    />
+                    <Route
+                      path="/editemployee/:employeeId/visa-details"
+                      element={<VisaDetails />}
+                    />
+                    <Route
+                      path="/editemployee/:employeeId/visa-details/:visaId/editvisadetails"
+                      element={<EditVisaDetails />}
+                    />
+                    <Route
+                      path="/editemployee/:employeeId/visa-details/add-visa-details"
+                      element={<AddVisaDetails />}
+                    />
+                    <Route
+                      path="/purchase-orders"
+                      element={<PurchaseOrders />}
+                    />
+                    <Route
+                      path="/change-password/:id"
+                      element={<ChangePasswordForm />}
+                    />
+                    <Route
+                      path="/addprospect"
+                      element={<AddProspectEmployee />}
+                    />
+                    <Route path="/candidates" element={<CandidateList />} />
+                    <Route path="/addcandidate" element={<AddCandidate />} />
+                    <Route
+                      path="/editcandidate/:candidateID"
+                      element={<EditCandidate />}
+                    />
+                    <Route path="/marketing" element={<MarketingList />} />
+                    <Route
+                      path="/marketing/editcandidate/:candidateID"
+                      element={<EditCandidate />}
+                    />
                   </>
-                ): role === "EMPLOYEE"? (
-                      <>
-                        <Route path="/change-password/:id" element={<ChangePasswordForm/>}/>
-                        <Route path="/" element={<EmployeeDetails/>} />
-                        <Route path="/trackings" element={<Tracking/>} />
-                        <Route path="/withholdSheet" element={<WithHoldSheet/>} />
-                      </>
-                    ): role === "PROSPECT"? (
-                      <>
-                        <Route path="/" element={<ProspetEmployee/>} />
-                        <Route path="/uploadDocs" element={<ProspectDocument/>}/>
-                        <Route path="/change-password/:id" element={<ChangePasswordForm/>}/>
-
-                      </>
-                    ):role === "RECRUITER"? (
-                      <>
-                        <Route path="/" element={<RecruiterDashboard/>} />
-                        <Route path="/marketing" element={<MarketingList/>}/>
-                        <Route path="/marketing/editcandidate/:candidateID" element={<EditCandidate/>}/>
-                        <Route path="/change-password/:id" element={<ChangePasswordForm/>}/>
-                      </>
-                    ):role === "SALES"? (
-                      <>
-                        <Route path="/candidates" element={<CandidateList/>}/>
-                        <Route path="/addcandidate" element={<AddCandidate/>}/>
-                        <Route path="/" element={<RecruiterDashboard/>} />
-                        <Route path="/editcandidate/:candidateID" element={<EditCandidate/>}/>
-                        <Route path="/marketing" element={<MarketingList/>}/>
-                        <Route path="/marketing/editcandidate/:candidateID" element={<EditCandidate/>}/>
-                        <Route path="/change-password/:id" element={<ChangePasswordForm/>}/>
-                      </>
-                    ):(
-                      <Route path="/*" element={<Navigate to="/login" />} />                          
-                   )
-          ):(
-             <Route path="/*" element={<Navigate to="/login" />} />
-          )}
-        </Routes>
+                ) : role === "EMPLOYEE" ? (
+                  <>
+                    <Route
+                      path="/change-password/:id"
+                      element={<ChangePasswordForm />}
+                    />
+                    <Route path="/" element={<EmployeeDetails />} />
+                    <Route path="/trackings" element={<Tracking />} />
+                    <Route path="/withholdSheet" element={<WithHoldSheet />} />
+                  </>
+                ) : role === "PROSPECT" ? (
+                  <>
+                    <Route path="/" element={<ProspetEmployee />} />
+                    <Route path="/uploadDocs" element={<ProspectDocument />} />
+                    <Route
+                      path="/change-password/:id"
+                      element={<ChangePasswordForm />}
+                    />
+                  </>
+                ) : role === "RECRUITER" ? (
+                  <>
+                    <Route path="/" element={<RecruiterDashboard />} />
+                    <Route path="/marketing" element={<MarketingList />} />
+                    <Route
+                      path="/marketing/editcandidate/:candidateID"
+                      element={<EditCandidate />}
+                    />
+                    <Route
+                      path="/change-password/:id"
+                      element={<ChangePasswordForm />}
+                    />
+                  </>
+                ) : role === "SALES" ? (
+                  <>
+                    <Route path="/candidates" element={<CandidateList />} />
+                    <Route path="/addcandidate" element={<AddCandidate />} />
+                    <Route path="/" element={<RecruiterDashboard />} />
+                    <Route
+                      path="/editcandidate/:candidateID"
+                      element={<EditCandidate />}
+                    />
+                    <Route path="/marketing" element={<MarketingList />} />
+                    <Route
+                      path="/marketing/editcandidate/:candidateID"
+                      element={<EditCandidate />}
+                    />
+                    <Route
+                      path="/change-password/:id"
+                      element={<ChangePasswordForm />}
+                    />
+                  </>
+                ) : (
+                  <Route path="/*" element={<Navigate to="/login" />} />
+                )
+              ) : (
+                <Route path="/*" element={<Navigate to="/login" />} />
+              )}
+            </Routes>
+          </div>
         </div>
-        </div>
-        
       </Router>
     </div>
   );
