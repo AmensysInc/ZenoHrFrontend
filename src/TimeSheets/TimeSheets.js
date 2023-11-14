fda6e08245da0360e6005c1a61e54d3342b2cd07
Sainath <92996163+sainathaluri@users.noreply.github.com>
2023-11-15 03:28:09 +0530

    Added api call to get data

--- src/TimeSheets/TimeSheets.js
+++ src/TimeSheets/TimeSheets.js
@@ -1,22 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { Select } from 'antd';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import React, { useEffect, useState } from "react";
+import { Select } from "antd";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { get, post } from "../SharedComponents/httpClient ";
 
 const { Option } = Select;
 
 export default function TimeSheets() {
-  const [selectedEmployee, setSelectedEmployee] = useState('');
-  const [selectedMonth, setSelectedMonth] = useState('');
-  const [selectedYear, setSelectedYear] = useState('');
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const [selectedEmployee, setSelectedEmployee] = useState("");
+  const [selectedProject, setSelectedProject] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
   const [timeSheets, setTimeSheets] = useState([]);
-  const [changedRecords, setChangedRecords] = useState([]); // New state variable for changed records
-  const getDayStyle = (params) =>
-  params.value && (params.value.getDay() === 0 || params.value.getDay() === 6) ? { color: 'red' } : {};
+  const [changedRecords, setChangedRecords] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    get("/employees")
+      .then((response) => {
+        const data = response.data;
+
+        if (data && data.content && Array.isArray(data.content)) {
+          setEmployees(data.content);
+        } else {
+          console.error("API response does not contain an array:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching employees:", error);
+      });
+  }, [apiUrl]);
+
+  useEffect(() => {
+    get(`employees/${selectedEmployee}/projects`)
+      .then((response) => {
+        const data = response.data;
+         console.log("Projects content:", data.content);
+        if (data && data.content && Array.isArray(data.content)) {
+          setProjects(data.content);
+        } else {
+          console.error("API response does not contain an array:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching projects:", error);
+      });
+  }, [selectedEmployee, apiUrl]);
+
+  // const getDayStyle = (params) =>
+  //   params.value && (params.value.getDay() === 0 || params.value.getDay() === 6)
+  //     ? { color: "red" }
+  //     : {};
+  const getDayStyle = (params) => {
+    if (params.value instanceof Date) {
+      return params.value.getDay() === 0 || params.value.getDay() === 6
+        ? { color: "red" }
+        : {};
+    }
+    return {};
+  };
 
   const getAbbreviatedDay = (date) => {
-    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return days[date.getDay()];
   };
 
@@ -25,7 +72,11 @@ export default function TimeSheets() {
     const endDate = new Date(year, month, 0);
 
     const datesArray = [];
-    for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
+    for (
+      let date = startDate;
+      date <= endDate;
+      date.setDate(date.getDate() + 1)
+    ) {
       datesArray.push(new Date(date));
     }
 
@@ -35,9 +86,7 @@ export default function TimeSheets() {
   const onCellValueChanged = (params) => {
     const { data, colDef, newValue } = params;
     const { field } = colDef;
-
-    // Check if the regularHours or overtimeHours field is changed
-    if (field === 'regularHours' || field === 'overtimeHours') {
+    if (field === "regularHours" || field === "overtimeHours") {
       const changedRecord = {
         employeeId: selectedEmployee,
         month: selectedMonth,
@@ -45,31 +94,41 @@ export default function TimeSheets() {
         date: data.date,
         [field]: newValue,
       };
-
-      // Check if the record is already in the changedRecords array
       const isRecordChanged = changedRecords.some(
-        (record) => record.employeeId === selectedEmployee && record.date === data.date
+        (record) =>
+          record.employeeId === selectedEmployee && record.date === data.date
       );
-
-      // Update or add the record to the changedRecords array
       setChangedRecords((prevRecords) =>
         isRecordChanged
           ? prevRecords.map((record) =>
-              record.employeeId === selectedEmployee && record.date === data.date ? changedRecord : record
+              record.employeeId === selectedEmployee &&
+              record.date === data.date
+                ? changedRecord
+                : record
             )
           : [...prevRecords, changedRecord]
       );
     }
 
-    console.log("Changed record : "+changedRecords);
+    console.log("Changed record : " + changedRecords);
   };
 
   const [gridOptions, setGridOptions] = useState({
     columnDefs: [
-      { headerName: 'Date', field: 'date', width: 150, cellStyle: getDayStyle },
-      { headerName: 'Day', field: 'day', width: 80 },
-      { headerName: 'Regular Hours', field: 'regularHours', width: 150, editable: true },
-      { headerName: 'Overtime Hours', field: 'overtimeHours', width: 150, editable: true },
+      { headerName: "Date", field: "date", width: 150, cellStyle: getDayStyle },
+      { headerName: "Day", field: "day", width: 80 },
+      {
+        headerName: "Regular Hours",
+        field: "regularHours",
+        width: 150,
+        editable: true,
+      },
+      {
+        headerName: "Overtime Hours",
+        field: "overTimeHours",
+        width: 150,
+        editable: true,
+      },
     ],
     defaultColDef: {
       editable: true,
@@ -78,74 +137,165 @@ export default function TimeSheets() {
     onCellValueChanged: onCellValueChanged,
   });
 
-  const generateRandomTimeSheets = (dates) => {
-    const timeSheetsArray = dates.map((date) => ({
-      date,
-      day: getAbbreviatedDay(date),
-      regularHours: Math.floor(Math.random() * 8) + 1,
-      overtimeHours: Math.floor(Math.random() * 4) + 1,
-    }));
-
-    return timeSheetsArray;
-  };
-
   useEffect(() => {
-    if (selectedEmployee && selectedMonth && selectedYear) {
+    if (selectedEmployee && selectedMonth && selectedYear && selectedProject) {
       const startDate = new Date(selectedYear, selectedMonth - 1, 1);
       const endDate = new Date(selectedYear, selectedMonth, 0);
       const datesInMonth = getDatesInMonth(selectedMonth, selectedYear);
-      const randomTimeSheets = generateRandomTimeSheets(datesInMonth);
-      setTimeSheets(randomTimeSheets);
+  
+      const requestBody = {
+        month: parseInt(selectedMonth, 10),
+        year: selectedYear,
+        employeeId: selectedEmployee,
+        projectId: selectedProject.projectId,
+      };
+      console.log(requestBody);
+  
+      post("/timesheets/getAllTimeSheets", requestBody)
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+        if (data && Array.isArray(data)) {
+          setTimeSheets(data);
+        } else {
+          console.error("API response does not contain an array:", data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching time sheets:", error);
+      });
     }
-  }, [selectedEmployee, selectedMonth, selectedYear]);
+  }, [selectedEmployee, selectedMonth, selectedYear, selectedProject]);
+  
 
   const handleSubmit = () => {
     // Implement the logic to submit the changed data
     // For example, you can make an API call to update the backend with the changes
 
     // For demonstration purposes, logging the changed records
-    console.log('Changed Records to Submit:', changedRecords);
+    console.log("Changed Records to Submit:", changedRecords);
 
     // Reset the changedRecords state after submission
     setChangedRecords([]);
   };
+  const monthOptions = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const startYear = 1990;
+  const endYear = 2099;
+  const yearOptions = [];
+
+  for (let year = startYear; year <= endYear; year++) {
+    yearOptions.push(year);
+  }
 
   return (
-    <div className="timesheets-container" style={{ marginLeft: '200px', maxWidth: '800px', width: '100%' }}>
+    <div
+      className="timesheets-container"
+      style={{ marginLeft: "200px", maxWidth: "800px", width: "100%" }}
+    >
       <div className="input-group">
         <div className="input-item">
           <label>Select Employee:</label>
-          <Select style={{ width: '150px' }} value={selectedEmployee} onChange={(value) => setSelectedEmployee(value)}>
-            <Option value="employee1">Employee 1</Option>
-            <Option value="employee2">Employee 2</Option>
-          </Select>
+          <select
+            className="form-control"
+            name="selectedEmployee"
+            value={selectedEmployee.employeeID}
+            onChange={(e) => setSelectedEmployee(e.target.value)}
+          >
+            <option value="">-- Select --</option>
+            {Array.isArray(employees) &&
+              employees.map((employee) => (
+                <option
+                  key={employee.employeeID}
+                  value={employee.employeeID}
+                >
+                  {employee.firstName} {employee.lastName}
+                </option>
+              ))}
+          </select>
         </div>
-
+        <div className="input-item">
+          <label>Select Project:</label>
+          <select
+            className="form-control"
+            name="selectedProject"
+            value={selectedProject ? selectedProject.projectId : ""}
+            onChange={(e) =>
+              setSelectedProject(
+                projects.find((project) => project.projectId === e.target.value)
+              )
+            }
+          >
+            <option value="">-- Select --</option>
+            {Array.isArray(projects) &&
+              projects.map((project) => (
+                <option key={project.projectId} value={project.projectId}>
+                  {project.subVendorOne}/{project.subVendorTwo}
+                </option>
+              ))}
+          </select>
+        </div>
         <div className="input-item">
           <label>Select Month:</label>
-          <Select style={{ width: '150px' }} value={selectedMonth} onChange={(value) => setSelectedMonth(value)}>
-            <Option value="1">January</Option>
-            <Option value="2">February</Option>
-            {/* Add more options as needed */}
+          <Select
+            style={{ width: "150px" }}
+            value={selectedMonth}
+            onChange={(value) => setSelectedMonth(value)}
+          >
+            {monthOptions.map((month, index) => (
+              <Option key={index + 1} value={(index + 1).toString()}>
+                {month}
+              </Option>
+            ))}
           </Select>
         </div>
 
         <div className="input-item">
           <label>Select Year:</label>
-          <Select style={{ width: '150px' }} value={selectedYear} onChange={(value) => setSelectedYear(value)}>
-            <Option value="2023">2023</Option>
-            <Option value="2024">2024</Option>
-            {/* Add more options as needed */}
+          <Select
+            style={{ width: "150px" }}
+            value={selectedYear}
+            onChange={(value) => setSelectedYear(value)}
+          >
+            {yearOptions.map((year) => (
+              <Option key={year} value={year.toString()}>
+                {year}
+              </Option>
+            ))}
           </Select>
         </div>
       </div>
 
-      <div className="ag-theme-alpine" style={{ height: '400px', width: '100%' }}>
-        <AgGridReact gridOptions={gridOptions} rowData={timeSheets}></AgGridReact>
+      <div
+        className="ag-theme-alpine"
+        style={{ height: "400px", width: "100%" }}
+      >
+        <AgGridReact
+          gridOptions={gridOptions}
+          rowData={timeSheets}
+        ></AgGridReact>
       </div>
-      <button onClick={handleSubmit} disabled={changedRecords.length === 0} type="submit" className="btn btn-outline-primary">
+      <button
+        onClick={handleSubmit}
+        disabled={changedRecords.length === 0}
+        type="submit"
+        className="btn btn-outline-primary"
+      >
         Submit
       </button>
     </div>
   );
-};
+}
